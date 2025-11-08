9a6ceeb3cd9f9b3ecb014aa30e169f9ad6d386fb
Hussain The MERN Guy <hussaintmerng@gmail.com>
2025-11-09 00:36:43 +0500

    Update route.js

--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -69,7 +69,7 @@ export async function POST(req) {
     );
 
     response.cookies.set("token", token, {
-      httpOnly: false,
+      httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       path: "/",
       maxAge: remember ? 60 * 60 * 24 * 7 : 60 * 60,
